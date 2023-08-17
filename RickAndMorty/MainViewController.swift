1815e161d20386daf26324b5d857f8cb8afe2b7c
Emre Özbağdatlı <74432853+EmreOzbagdatli@users.noreply.github.com>
2023-08-17 23:05:55 +0300

    Initial commit

---
+++ RickAndMorty/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  RickAndMorty
+//
+//  Created by Emre Özbağdatlı on 17.08.2023.
+//
+
+import SwiftUI
+
+struct MainViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        MainViewController()
+    }
+}
